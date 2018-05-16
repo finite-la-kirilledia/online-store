8d40908fa63e8f57ff84adca5d09f2b87f1081a9
kirillminko <kirill040994@gmail.com>
2018-05-16 23:07:00 +0300

    * add STATIC_ROOT for pythonanywhere.com

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = '/home/onlinestore/online-store/static'
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
